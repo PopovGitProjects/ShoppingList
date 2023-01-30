044f889e09facdd1b97ad611e3c32620a9a18f79
PopovGitProjects <popov.workmail@gmail.com>
2023-01-30 17:56:11 +0500

    Data layer is added

--- app/src/main/java/com/popov/shoppinglist/domain/repository/ShopListRepository.kt
+++ app/src/main/java/com/popov/shoppinglist/domain/repository/ShopListRepository.kt
@@ -6,7 +6,7 @@ interface ShopListRepository {
     fun getShopList(): List<ShopItem>
     fun getShopItem(shopItemId: Int): ShopItem
     fun addShopItem(shopItem: ShopItem)
-    fun editShopItemUseCase(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
     fun deleteShopItem(shopItem: ShopItem)
 
 }
--- app/src/main/java/com/popov/shoppinglist/domain/usecases/EditShopItemUseCase.kt
+++ app/src/main/java/com/popov/shoppinglist/domain/usecases/EditShopItemUseCase.kt
@@ -4,7 +4,7 @@ import com.popov.shoppinglist.domain.models.ShopItem
 import com.popov.shoppinglist.domain.repository.ShopListRepository
 
 class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
-    fun editShopItemUseCase(shopItem: ShopItem) {
+    fun editShopItem(shopItem: ShopItem) {
         shopListRepository.editShopItemUseCase(shopItem)
     }
 }
